ee89aca4f77b7f69256e9f512d50c5bb1f0f2ec2
YounesN <younes@wayne.edu>
2017-08-02 02:52:35 -0400

    added sqlite

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Storage } from '@ionic/storage';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, private storage: Storage) {
-    
+  pushup: number;
+  situp: number;
+  squat: number;
+  sqlstorage: any = null;
+
+  constructor(public navCtrl: NavController) {
+    this.sqlstorage = new SQLite();
+    this.sqlstorage.create({
+      name: 'data.db',
+      location: 'default'
+    })
+    .then((db: SQLiteObject) => {
+      db.executeSql('CREATE TABLE IF NOT EXISTS workouts(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, date datetime NOT NULL DEFAULT GETDATE(), pushup SMALLINT DEFAULT(0), situp SMALLINT DEFAULT(0), squat SMALLINT DEFAULT(0))', {})
+      .then(() => console.log('Executed SQL'))
+      .catch(e => console.log(e));
+    })
+    .catch(e => console.log(e));
+  }
+
+  addWorkout()
+  {
+    let q = "INSERT INTO workouts(pushup, situp, squat) VALUES (" + this.pushup + ", " + this.situp + ", " + this.squat + ")";
+    console.log(q);
+    this.sqlstorage.executeSql(q);
   }
 }
